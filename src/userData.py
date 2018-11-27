b2ea85bc4b00e1393e18b1981f9940e6a5c20ed6
evapodaca87 <evapodaca87@gmail.com>
2018-11-27 11:39:23 -0700

    started adding seed data for db

---
+++ src/userData.py
@@ -0,0 +1,44 @@
+{
+    "data": [
+        {
+            "userId": 1,
+            "firstName": "Evan",
+            "lastName": "Apodaca",
+            "email": "evapodaca87@gmail.com",
+            "username": "evapodaca87",
+            "password": "evan"
+        },
+        {
+            "userId": 2,
+            "firstName": "Bill",
+            "lastName": "Bain",
+            "email": "bill@mail.com",
+            "username": "billbain",
+            "password": "bain"
+        },
+        {
+            "userId": 3,
+            "firstName": "Jody",
+            "lastName": "Isaguirre",
+            "email": "jody@mail.com",
+            "username": "jodyisaquirre",
+            "password": "irre"
+        },
+        {
+            "userId": 4,
+            "firstName": "Jonathan",
+            "lastName": "Hovland",
+            "email": "jonathan@mail.com",
+            "username": "jonathanhovland",
+            "password": "land"
+        },
+        {
+            "userId": 5,
+            "firstName": "Rob",
+            "lastName": "Hill",
+            "email": "robhill@mail.com",
+            "username": "robhill",
+            "password": "hill"
+        }
+    ]
+}
